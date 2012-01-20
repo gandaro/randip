3607e94c481ce5dd53f929e7003dc65a88ee54b0
Jakob Kramer <jakob.kramer@gmx.de>
2012-01-20 20:35:30 +0100

    use 'BLOCKS' macro; fill blocks with zeros

--- randip6.c
+++ randip6.c
@@ -2,16 +2,18 @@
 # include <assert.h>
 # include <stdint.h>
 
+# define BLOCKS 4
+
 int main(void)
 {
 	FILE *random = fopen("/dev/urandom","r");
-	uint16_t ip_s[4];
+	uint16_t ip_s[BLOCKS];
 
 	assert(random != NULL);
-	assert(fread(ip_s,2,4,random) == 4); // read 4 * 2 random bytes
+	assert(fread(ip_s,2,BLOCKS,random) == BLOCKS); // read 4 * 2 random bytes
 
 	fclose(random);
 
-	for (int i = 0; i < 4; ++i)
-		printf(":%x",ip_s[i]);
+	for (int i = 0; i < BLOCKS; ++i)
+		printf(":%04x",ip_s[i]);
 }
