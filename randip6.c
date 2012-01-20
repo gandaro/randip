5b5d054e0a8cbd410ecb72837c66b03623caf399
lbo <lbo@spheniscida.de>
2012-01-20 13:36:47 +0100

    Initial Commit

---
+++ randip6.c
@@ -0,0 +1,28 @@
+# include <stdio.h>
+# include <stdlib.h>
+# include <fcntl.h>
+# include <unistd.h>
+# include <assert.h>
+# include <stdint.h>
+
+int main(void)
+{
+	int rfd, i = 0;
+	uint16_t ip_s[4];
+
+	rfd = open("/dev/urandom",O_RDONLY);
+
+	assert(rfd >= 0);
+	
+	for ( ; i < 4; ++i )
+	{
+		read(rfd,ip_s+i,2);
+		printf(":%x",ip_s[i]);
+	}
+	
+	printf("\n");
+
+	close(rfd);
+
+	return 0;
+}
